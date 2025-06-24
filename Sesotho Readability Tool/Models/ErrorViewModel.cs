16106bd44ab0adfd531d5d915b0f926e6473e01c
s224032194 <s224032194@M104-18>
2025-06-24 02:05:06 +0200

    Add project files.

---
+++ Sesotho Readability Tool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sesotho_Readability_Tool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
